9a57e5e04b4833b4b331b0cd1420df01cdb27864
Juan Manuel Garavito Facundo <juanma.gf4@hotmail.com>
2023-12-22 10:23:45 -0500

    Add Nuget Packages and Startup

---
+++ DesafioProsegur.API/Startup.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace DesafioProsegur.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        }
+    }
+}
